c332b64bb6ed737430616ab1309320fe85d74e69
mynameiscun <86701390+mynameiscun@users.noreply.github.com>
2021-11-20 18:13:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "ò ó o",
-    introTitle: 'Chang ới',
+    introTitle: 'cậu ới',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim tui lẻ loi
